1021bbf87bc7ab7f02b9dad23ebc03a6471d7ed7
Maria <mariahcdcosta@gmail.com>
2019-09-09 23:02:10 +0400

    Display film statistics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,114 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular8-star-wars';
+  apiUrl = environment.api_url;
+  result = {
+    longestCrawl: '',
+    mostCharacterAppearances: '',
+    topSpecies: []
+  };
+  isLoading: boolean;
+  films = [];
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Function that executes when the button is clicked
+   */
+  submit() {
+    this.isLoading = true;
+    this.getFilms();
+  }
+
+  /**
+   * Send an API call to retrieve a list of films
+   */
+  getFilms() {
+    this.http.get(this.apiUrl + 'films').subscribe(
+      (response: any) => {
+        this.films = response.results;
+        this.getFilmStatistics();
+      }
+    );
+  }
+
+  /**
+   * Calculate film statistics
+   */
+  async getFilmStatistics() {
+    let maxLength = 0;
+    const characterMap = {};
+    const speciesMap = [];
+    this.films.forEach((film: any) => {
+
+      // Track the film with longest opening crawl
+      const length = film.opening_crawl.length;
+      if (length > maxLength) {
+        maxLength = length;
+        this.result.longestCrawl = film.title;
+      }
+
+      // Track each character's appearance in the film
+      film.characters.forEach((character: string) => {
+        characterMap[character] = characterMap.hasOwnProperty(character) ? ++characterMap[character] : 1;
+      });
+
+      // Track the appearance of each species in the film
+      film.species.forEach((species: string) => {
+        const existing = speciesMap.filter((s) => s.url === species)[0];
+        if (existing) {
+          ++existing.count;
+        } else {
+          speciesMap.push({
+            url: species,
+            count: 1
+          });
+        }
+      });
+    });
+
+    this.getMostCharacterAppearances(characterMap);
+    this.getMostSpeciesAppearances(speciesMap);
+  }
+
+  /**
+   * Determine the character with the most number of film appearances
+   * @param characterMap object with format { url: count_of_appearances }
+   */
+  async getMostCharacterAppearances(characterMap: any) {
+    this.result.mostCharacterAppearances = (await this.getItem(Object.keys(characterMap).reduce(
+      (a, b) => characterMap[a] > characterMap[b]
+        ? a : b))).name;
+  }
+
+  /**
+   * Determine the species with the most number of film appearances
+   * @param speciesMap array containing each species url and appearance count
+   */
+  getMostSpeciesAppearances(speciesMap: any) {
+    speciesMap.sort((a, b) => (a.count < b.count) ? 1 : -1);
+
+    speciesMap.slice(0, 3).forEach(async (species) => {
+      this.result.topSpecies.push({
+        name: (await this.getItem(species.url)).name,
+        number_of_appearances: species.count
+      });
+    });
+    this.isLoading = false;
+  }
+
+  /**
+   * Send an API GET request to fetch an item
+   * @param url The url of the GET request
+   */
+  async getItem(url: string): Promise<any> {
+    return await this.http.get(url).toPromise();
+  }
+
 }
