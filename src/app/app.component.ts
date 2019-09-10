c5c525dc1ddbdd51ff050d68cb5d178e201ba3d2
Maria <mariahcdcosta@gmail.com>
2019-09-11 00:02:36 +0400

    Display planet providing the most vehicle pilots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { Angular2SwapiService } from 'angular2-swapi';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,30 +11,24 @@ export class AppComponent {
   result = {
     longestCrawl: '',
     mostCharacterAppearances: '',
-    topSpecies: []
+    topSpecies: [],
+    topPilotProvider: null
   };
   isLoading: boolean;
   films = [];
 
   constructor(
-    private http: HttpClient,
-    private swapi: Angular2SwapiService
+    private http: HttpClient
   ) { }
 
   /**
    * Function that executes when the button is clicked
    */
-  submit() {
+  async submit() {
     this.isLoading = true;
-
-    this.swapi.getFilms().toPromise().then(
-      (films) => {
-        this.films = films;
-      }
-    ).then(() => {
-      this.getFilmStatistics();
-      this.isLoading = false;
-    });
+    this.films = (await this.sendRequest(this.apiUrl + 'films')).results;
+    this.getFilmStatistics();
+    this.getPlanetStatistics();
   }
 
   /**
@@ -82,7 +75,7 @@ export class AppComponent {
    * @param characterMap object with format { url: count_of_appearances }
    */
   async getMostCharacterAppearances(characterMap: any) {
-    this.result.mostCharacterAppearances = (await this.getItem(Object.keys(characterMap).reduce(
+    this.result.mostCharacterAppearances = (await this.sendRequest(Object.keys(characterMap).reduce(
       (a, b) => characterMap[a] > characterMap[b]
         ? a : b))).name;
   }
@@ -96,17 +89,61 @@ export class AppComponent {
 
     speciesMap.slice(0, 3).forEach(async (species) => {
       this.result.topSpecies.push({
-        name: (await this.getItem(species.url)).name,
+        name: (await this.sendRequest(species.url)).name,
         number_of_appearances: species.count
       });
     });
   }
 
   /**
-   * Send an API GET request to fetch an item
+   * Determine the planet that provided the largest number of vehicle pilots
+   */
+  async getPlanetStatistics() {
+    const planetMap = {};
+    let page = 1;
+
+    while (page !== null) {
+      const response = await this.sendRequest(this.apiUrl + 'people?page=' + page);
+      response.results.forEach(async (person) => {
+        if (person.vehicles.length === 0) {
+          return;
+        }
+        const homeworld = (await this.sendRequest(person.homeworld));
+        const species = (await this.sendRequest(person.species[0])).name;
+        if (planetMap[homeworld.url]) {
+          ++planetMap[homeworld.url].number_of_pilots;
+          if (!planetMap[homeworld.url].pilots.filter(p => p.name === person.name).length) {
+            planetMap[homeworld.url].pilots.push({
+              name: person.name,
+              species
+            });
+          }
+        } else {
+          planetMap[homeworld.url] = {
+            planet: homeworld.name,
+            number_of_pilots: 1,
+            pilots: [
+              {
+                name: person.name,
+                species
+              }
+            ]
+          };
+        }
+      });
+      page = response.next ? ++page : null;
+    }
+
+    const planets = Object.values(planetMap);
+    this.result.topPilotProvider = planets.reduce((a: any, b: any) => a.number_of_pilots > b.number_of_pilots ? a : b);
+    this.isLoading = false;
+  }
+
+  /**
+   * Send an API GET request to fetch data
    * @param url The url of the GET request
    */
-  async getItem(url: string): Promise<any> {
+  async sendRequest(url: string): Promise<any> {
     return await this.http.get(url).toPromise();
   }
 
